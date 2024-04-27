34e30e384bab9acf6763f877b44e7da11ee3fe79
Rodolfo Silva <reedysilvabr@gmail.com>
2024-04-27 13:28:19 -0300

    create model to struct

---
+++ TableViewProgrammatically/TableViewProgrammatically/Applicatiion/TableView/Model/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  TableViewProgrammatically
+//
+//  Created by rodolfo silva on 27/04/24.
+//
+
+import UIKit
+
+// Estrutura para representar cada item na tabela.
+struct TableViewItens {
+    let title: String       // Título do item.
+    let iconName: String    // Nome do ícone a ser exibido ao lado do título.
+    let showArrow: Bool     // Booleano para mostrar ou ocultar a seta.
+}
